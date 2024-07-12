a803674b8f75cd7ea0d32cb077252f9824b82084
Prakhar <117335249+prakhar6585@users.noreply.github.com>
2024-07-12 12:47:35 +0530

    bcrypt changes

--- Server/helpers/authHelper.js
+++ Server/helpers/authHelper.js
@@ -1,15 +1,15 @@
-import bcrypt from 'bcrypt'; 
+import bcrypt from 'bcryptjs';
 
 export const hashPassword = async (password) => {
     try {
-        const saltRounds=10;
-        const hashedPassword = await bcrypt.hash(password,saltRounds);
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         return hashedPassword;
     } catch (error) {
         console.log(`Error during hashing pswrd ${error}`)
     }
 }
 
-export const comparePassword = async (password,hashedPassword) => {
-    return bcrypt.compare(password,hashedPassword);
+export const comparePassword = async (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword);
 }
